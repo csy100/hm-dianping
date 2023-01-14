4ca89eeb05034790d7b93cc863140aa603c230d7
csy <csy@100>
2023-01-14 22:47:07 +0800

    登录拦截器

--- hm-dianping/src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ hm-dianping/src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,22 +15,24 @@ public class LoginInterceptor implements HandlerInterceptor {
         HttpSession session = request.getSession();
 
         // 2. 从session中取到用户
-        User user = (User) session.getAttribute("user");
+        UserDTO user = (UserDTO) session.getAttribute("user");
 
         // 3. 判断用户是否存在
         if (user == null) {
-            // 4. 进行拦截
+            // 4. 设置状态码，状态码 401 Unauthorized
+            // 代表客户端错误，指的是由于缺乏目标资源要求的身份验证凭证，发送的请求未得到满足
+            response.setStatus(401);
             return false;
         }
 
         // 5. 用户存在，将用户存在ThreadLocal中，开辟线程
-
-
+        UserHolder.saveUser(user);
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
+        // 移除用户，避免内存的泄露
+        UserHolder.removeUser();
     }
 }
