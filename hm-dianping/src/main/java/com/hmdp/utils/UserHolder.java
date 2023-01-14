ee56e3e6054627d4eeb36e8aa23f03c444e70819
csy <csy@100>
2023-01-14 22:42:01 +0800

    change

--- hm-dianping/src/main/java/com/hmdp/utils/UserHolder.java
+++ hm-dianping/src/main/java/com/hmdp/utils/UserHolder.java
@@ -1,16 +1,15 @@
 package com.hmdp.utils;
 
 import com.hmdp.dto.UserDTO;
-import com.hmdp.entity.User;
 
 public class UserHolder {
-    private static final ThreadLocal<User> threadLocal = new ThreadLocal<>();
+    private static final ThreadLocal<UserDTO> threadLocal = new ThreadLocal<>();
 
-    public static void saveUser(User user){
+    public static void saveUser(UserDTO user){
         threadLocal.set(user);
     }
 
-    public static User getUser(){
+    public static UserDTO getUser(){
         return threadLocal.get();
     }
 
