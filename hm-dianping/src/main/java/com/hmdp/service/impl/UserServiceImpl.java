9f3e0023176c3897f46d96ad5f9995ada8904e92
csy <csy@100>
2023-01-14 20:57:39 +0800

    login service

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
 /**
  * <p>
  * 服务实现类
@@ -62,7 +64,42 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
      */
     @Override
     public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1. 校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2. 不符合，直接返回
+            return Result.fail("手机格式错误");
+        }
 
-        return null;
+        // 3. 手机号校验成功，校验验证码
+        String code = loginForm.getCode(); //前端返回的验证码
+        Object cacheCode = session.getAttribute("code"); // 保存在session中的验证码
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            //session中的验证码为空或者二者比对不成功
+            return Result.fail("验证码错误！");
+        }
+
+        // 4. 验证码也校验成功，查询用户
+        User user = query().eq("phone", phone).one();
+
+        // 5. 若用户不存在，则直接创建新的用户即可，并保存至数据库中
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+
+        // 6. 保存用户到session中
+        session.setAttribute("user", user);
+
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        // 生成随机nickName
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomNumbers(10));
+        // 保存用户到数据库中
+        this.save(user);
+        return user;
     }
 }
