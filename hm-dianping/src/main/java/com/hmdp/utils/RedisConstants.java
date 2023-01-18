0934afdaac7e46c242bac976ac748414819a819e
csy <csy@100>
2023-01-18 09:07:58 +0800

    change

--- hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
@@ -9,7 +9,6 @@ public class RedisConstants {
     public static final Long CACHE_NULL_TTL = 2L;
 
     public static final String CACHE_SHOP_KEY = "cache:shop:";
-
     public static final Long CACHE_SHOP_TTL = 30L;
 
     public static final String LOCK_SHOP_KEY = "lock:shop:";
